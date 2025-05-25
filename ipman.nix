7c19139d81ce6607c91ba80cf9a1ef31d73cb317
plan9better <plan9better@proton.me>
2025-05-25 17:24:58 +0200

    v0.0.5

--- ipman.nix
+++ ipman.nix
@@ -3,8 +3,8 @@
     chart = lib.helm.downloadHelmChart {
       repo = "https://dialohq.github.io/ipman";
       chart = "ipman";
-      version = "0.0.4";
-      chartHash = "sha256-p+jMzmfhUvyIFN+7F2BaJchrB/Uvu0OqWEnh5LsgUT8=";
+      version = "0.0.5";
+      chartHash = "sha256-NZ7xQrIFtJlCQa9c7b2s3cGCxYTY3bp7rnpfVF8UXqM=";
     };
   };
 }
