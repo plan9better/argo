1eb056f4a154bfbcd455003844838bcee908eaec
plan9better <plan9better@proton.me>
2025-05-27 15:20:13 +0200

    bump ipman to v0.0.7-2

--- ipman.nix
+++ ipman.nix
@@ -3,8 +3,8 @@
     chart = lib.helm.downloadHelmChart {
       repo = "https://dialohq.github.io/ipman";
       chart = "ipman";
-      version = "0.0.7-1";
-      chartHash = "sha256-C+g4pvqfsmj/tyQHW1fagmlWo92Ty2KUTkJIVKU0Jh8=";
+      version = "0.0.7-2";
+      chartHash = "sha256-hWMkNm3jawvreMpxmYw2qJfTQ0fo+RwqM2jnhFfGQZY=";
     };
   };
 }
