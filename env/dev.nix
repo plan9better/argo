2b672d03c20dd24458994267dc16713aa176455b
plan9better <plan9better@proton.me>
2025-07-28 15:08:55 +0200

    fix: add namespace

--- env/dev.nix
+++ env/dev.nix
@@ -7,6 +7,14 @@
     ../k8s/crds/vlanman.nix
   ];
   applications.vlanman = {
+    namespace = "vlanman";
+    resources.namespaces.vlanman.metadata.labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged";
+      "pod-security.kubernetes.io/enforce-version" = "latest";
+      "pod-security.kubernetes.io/warn" = "restricted";
+      "pod-security.kubernetes.io/warn-version" = "latest";
+    };
+
     imports = [((import ./vlanman.nix) {inherit lib;})];
   };
 }
