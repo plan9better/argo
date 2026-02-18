f271d7fc7f70f67b2229b4b30989e9a14f5c7e4d
plan9better <plan9better@proton.me>
2026-02-18 15:49:24 +0100

    test vlanman swap

--- env/vlanman.nix
+++ env/vlanman.nix
@@ -1,6 +1,6 @@
 {lib, ...}: {
   helm.releases.vlanman = {
-    chart = ../vlanman-old.tgz;
+    chart = ../vlanman-0.0.0.tgz;
     # chart = lib.helm.downloadHelmChart {
     #   repo = "https://dialohq.github.io/vlanman";
     #   chart = "vlanman";
